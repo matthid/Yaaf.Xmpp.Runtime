56735de3846bc077130f078b97f5068055093716
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-13 14:17:40 +0100

    add script to test component port.

--- src/source/Yaaf.Xmpp.Runtime/Script.fsx
+++ src/source/Yaaf.Xmpp.Runtime/Script.fsx
@@ -2,4 +2,25 @@
 // einen Leitfaden zum Programmieren in F#.
 
 open System.IO
+open System
+open System.Threading;
+open System.Net
+open System.Net.Sockets
 
+let listener = new TcpListener(IPAddress.Any, 15347)
+listener.Start()
+let acceptor () =
+  while true do
+    let client = listener.AcceptTcpClient()
+    printfn "Connection registered!"
+    let stream = client.GetStream()
+    let reader = new StreamReader(stream)
+    let writer () =
+      let mutable run = true
+      while run do
+        let data = reader.ReadLine()
+        if data = null then run <- false
+        else printfn "READ: %s" data
+    Thread(writer).Start()
+
+Thread(acceptor).Start()
