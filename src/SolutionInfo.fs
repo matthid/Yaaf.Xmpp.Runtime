40d0dc9e28ff30c5a253fad7f0e4e6b5bf43019d
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-27 00:06:03 +0100

    Bump version to 0.0.7

--- src/SolutionInfo.fs
+++ src/SolutionInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.Xmpp.Runtime")>]
 [<assembly: AssemblyProductAttribute("Yaaf.Xmpp.Runtime")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.Xmpp.Runtime Copyright © Matthias Dittrich 2015")>]
-[<assembly: AssemblyVersionAttribute("0.0.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.6")>]
+[<assembly: AssemblyVersionAttribute("0.0.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.7")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.6"
+    let [<Literal>] Version = "0.0.7"
