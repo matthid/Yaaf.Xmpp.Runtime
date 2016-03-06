ea674008945d27778adc2939e3c053bfd52f745e
Matthias Dittrich <matthi.d@gmail.com>
2016-03-06 15:38:34 +0100

    fix nunit3 failure

---
+++ src/test/Test.Yaaf.Xmpp/Tests.fs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// ----------------------------------------------------------------------------
+namespace Test.Yaaf.Xmpp
+
+open NUnit.Framework
+
+[<TestFixture>]
+type Tests() =
+  [<Test>]
+  member this.``Empty test to make nunit3 empty`` () = ()
